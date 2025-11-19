f821327693a56f95664e04e9efebd781903837a4
almirAlmir <aasc@cin.ufpe.br>
2025-11-19 00:02:00 -0300

    funcao get chord notes que aplica tambem a modulacao escolhida

--- api_server.py
+++ api_server.py
@@ -1,3 +1,23 @@
 from flask import Flask, request, jsonify
 from chordLogic import get_triade, apply_modulation
 from synthConfig import KEY_BASE_MIDI, ENVELOPE, FORMA_ONDA, REVERB, DELAY
+
+app = Flask(__name__)
+
+@app.route('api/chord/<int:grau>', methods=['GET'])
+def get_chord_notes(grau):
+    
+    tipo_modulacao = request.args.get('mod') #pega o tipo da mudulaçao
+
+    acorde_base = get_triade(grau, KEY_BASE_MIDI)
+    acorde_final = acorde_base
+
+    #A funcao de modulacao entra aqui
+    if tipo_modulacao:
+        acorde_final = apply_modulation(acorde_final, tipo_modulacao)
+
+    return jsonify({
+        'grau': grau,
+        'modulacao': tipo_modulacao or 'None',
+        'notas_midi': acorde_final,
+        'status': 'success'})    
